8d3352a91293528e4106babc7980b177ab66a139
Luan Nguyen <luannguyenbkit@gmail.com>
2025-01-03 22:41:46 +0700

    feat: add validator

--- src/alarms/presenters/http/alarms.controller.ts
+++ src/alarms/presenters/http/alarms.controller.ts
@@ -2,11 +2,18 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AlarmsService } from '../../application/alarms.service';
 import { CreateAlarmDto } from './dto/create-alarm.dto';
 import { CreateAlarmCommand } from 'src/alarms/application/commands/create-alarm.command';
+import { ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
-@Controller('alarms')
+@Controller({
+  path: 'alarms',
+  version: '1',
+})
 export class AlarmsController {
   constructor(private readonly alarmsService: AlarmsService) {}
 
+  @ApiOperation({ summary: 'Create an alarm' })
+  @ApiResponse({ status: 201, description: 'Alarm created successfully' })
+  @ApiBody({ type: CreateAlarmDto })
   @Post()
   create(@Body() createAlarmDto: CreateAlarmDto) {
     return this.alarmsService.create(
@@ -14,6 +21,8 @@ export class AlarmsController {
     );
   }
 
+  @ApiOperation({ summary: 'Get all alarms' })
+  @ApiResponse({ status: 200, description: 'Alarms fetched successfully' })
   @Get()
   findAll() {
     return this.alarmsService.findAll();
--- src/alarms/presenters/http/dto/create-alarm.dto.ts
+++ src/alarms/presenters/http/dto/create-alarm.dto.ts
@@ -1,4 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateAlarmDto {
+  @ApiProperty()
+  @IsNotEmpty()
   name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
   severity: string;
 }
