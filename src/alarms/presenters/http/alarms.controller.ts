1e657a99cbbd5fb56507203dc4d434555264c924
Luan Nguyen <luannguyenbkit@gmail.com>
2025-01-04 22:34:23 +0700

    feat: add validator

--- src/alarms/presenters/http/alarms.controller.ts
+++ src/alarms/presenters/http/alarms.controller.ts
@@ -3,6 +3,7 @@ import { AlarmsService } from '../../application/alarms.service';
 import { CreateAlarmDto } from './dto/create-alarm.dto';
 import { CreateAlarmCommand } from 'src/alarms/application/commands/create-alarm.command';
 import { ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import { Alarm } from 'src/alarms/domain/alarm';
 
 @Controller({
   path: 'alarms',
@@ -12,19 +13,27 @@ export class AlarmsController {
   constructor(private readonly alarmsService: AlarmsService) {}
 
   @ApiOperation({ summary: 'Create an alarm' })
-  @ApiResponse({ status: 201, description: 'Alarm created successfully' })
+  @ApiResponse({
+    status: 201,
+    description: 'Alarm created successfully',
+    type: Alarm,
+  })
   @ApiBody({ type: CreateAlarmDto })
   @Post()
-  create(@Body() createAlarmDto: CreateAlarmDto) {
+  create(@Body() createAlarmDto: CreateAlarmDto): Promise<Alarm> {
     return this.alarmsService.create(
       new CreateAlarmCommand(createAlarmDto.name, createAlarmDto.severity),
     );
   }
 
   @ApiOperation({ summary: 'Get all alarms' })
-  @ApiResponse({ status: 200, description: 'Alarms fetched successfully' })
+  @ApiResponse({
+    status: 200,
+    description: 'Alarms fetched successfully',
+    type: Alarm,
+  })
   @Get()
-  findAll() {
+  findAll(): Promise<Alarm[]> {
     return this.alarmsService.findAll();
   }
 }
